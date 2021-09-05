aecfcd834cf76674727ffa1bde965ffaa82601c9
gidmak <gideonmarkus@gmail.com>
2021-09-05 06:46:44 +0100

    Easy level

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using System.Linq.Expressions;
+using System.Data;
+using System.Security.Cryptography;
+using System;
 
 namespace NumberGuess
 {
@@ -6,7 +9,61 @@ namespace NumberGuess
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("= = = = = NUMBER GUESSING GAME = = = = =");
+            
+            GetLevel();    
         }
+
+        private static void GetLevel()
+        {
+            int level = 0;
+            try
+            {
+                level = int.Parse(Console.ReadLine());
+                
+                Easy();
+              
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("You did not enter a valid level");
+            }
+        }
+        private static void Easy()
+        {
+            Console.WriteLine("EASY");
+            Console.WriteLine("Choose a random number from 1 to  10. You have up to 6 trials....");
+            int secret = new Random().Next(1,10);
+
+            int allowedTry = 6;
+            int noOfTry = 0;
+            
+            while(true)
+            {
+                int guess = int.Parse(Console.ReadLine());
+                noOfTry++;
+                if(guess == secret)
+                {
+                    Console.Write("Congratulations. You made it in " + noOfTry + " trials");
+                    break;
+                }
+                if(guess < secret)
+                {
+                    Console.Write("Too low");
+                }
+                if(guess > secret)
+                {
+                    Console.Write("Too high");
+                }
+                if(noOfTry == allowedTry)
+                {
+                    Console.Write($"The number was: {secret}");
+                    break;
+                }
+                Console.WriteLine($" You have {allowedTry - noOfTry} tries left. Enter another number: ");
+            }
+        }
+                
+       
     }
 }
